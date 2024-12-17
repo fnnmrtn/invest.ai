2b1fcd4560c220b918e7db761ad4c43abaf68f73
F0xhopper <you@example.com>
2024-12-17 23:11:07 +0000

    Sending requests to OPENAI - 429 Res

--- src/script.js
+++ src/script.js
@@ -8,25 +8,28 @@ const timeRangeInput = document.getElementById("timeRangeInput");
 const submitButton = document.getElementById("submitButton");
 const resultContainer = document.getElementById("resultContainer");
 const resultText = document.getElementById("resultText");
-
-// const openaiApiKey = process.env["API_KEY"];
+const openaiApiKey = "API_KEY";
 
 const processSubmission = async function () {
-  //   const repsonse = await fetch("https://api.openai.com/v1/chat/completions", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       Authorization: ⁠ Bearer ${openaiApiKey}"⁠,
-  //     },
-  //     body: json.stringify({
-  //       model: "gpt-4o-mini",
-  //       messages: [{ role: "user", content: "Say this is a test!" }],
-  //       temperature: 0.7,
-  //     }),
-  //   });
-  //   const data = await repsonse.json();
+  const repsonse = await fetch("https://api.openai.com/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${openaiApiKey}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "user", content: "What is the most popular fruit in India." },
+      ],
+      temperature: 0.7,
+    }),
+  });
+  const data = await repsonse.json();
+  console.log(data);
 };
 
 submitButton.addEventListener("click", () => {
-  console.log("test");
+  console.log("Submit button clicked!");
+  processSubmission();
 });
